f7d9a937de5ba920291bb815fc21d28f98ab851c
henrynester <henrynester@gmail.com>
2021-08-08 17:40:05 -0400

    nothin

--- can_messages.h
+++ can_messages.h
@@ -6,7 +6,7 @@
  */
 
 #ifndef CAN_MESSAGES_H
-#define	CAN_MESSAGES_H
+#define CAN_MESSAGES_H
 
 #include <stdint.h>
 
@@ -22,35 +22,40 @@
 #define RCU_ID_POWER_PYRO_RCU 0x04
 
 #define ID_PYRO_CONTROL 0x50
-struct PyroControl_t {
-    uint8_t fire_ignitor:1;
-    uint8_t fire_main:1;
-    uint8_t fire_drogue:1;
+struct PyroControl_t
+{
+    uint8_t fire_ignitor : 1;
+    uint8_t fire_main : 1;
+    uint8_t fire_drogue : 1;
 };
 
 #define ID_VALVE_CONTROL 0x100
 
-struct ValveControl_t {
+struct ValveControl_t
+{
     uint8_t main_ox_valve_goal_pos;
     uint8_t fuel_pres_valve_goal_pos;
-    struct SolenoidsControl_t {
-    uint8_t engine_vent_valve_close : 1;
-    uint8_t main_fuel_valve_open : 1;
-    uint8_t aux_1_energize : 1;
-    uint8_t aux_2_energize : 1;
-} solenoids;
+    struct SolenoidsControl_t
+    {
+        uint8_t engine_vent_valve_close : 1;
+        uint8_t main_fuel_valve_open : 1;
+        uint8_t aux_1_energize : 1;
+        uint8_t aux_2_energize : 1;
+    } solenoids;
 };
 
 #define ID_ENGINE_SENSORS_A 0x110
-struct EngineSensorsA_t{
+struct EngineSensorsA_t
+{
     uint16_t thrust_raw;
     uint16_t chamber_press_raw;
     uint16_t fuel_inj_press_raw;
     uint16_t ox_inj_press_raw;
-} ;
+};
 
 #define ID_ENGINE_SENSORS_B 0x120
-struct EngineSensorsB_t {
+struct EngineSensorsB_t
+{
     uint16_t fuel_tank_press_raw;
     uint16_t aux_1_raw;
     uint16_t aux_2_raw;
@@ -59,69 +64,78 @@ struct EngineSensorsB_t {
 
 #define ID_OX_MAIN_MOTOR_STATUS 0x200
 #define ID_FUEL_PRESS_MOTOR_STATUS 0x210
-struct MotorStatus_t {
+struct MotorStatus_t
+{
     uint8_t pos;
     int8_t speed;
-    struct LimitSwitchStatus_t {
+    struct LimitSwitchStatus_t
+    {
         uint8_t limit_switch : 1;
         uint8_t has_homed : 1;
     } homing;
-    struct MotorValveFaults_t {
+    struct MotorValveFaults_t
+    {
         uint8_t motor_stuck : 1;
         uint8_t homing_timeout : 1;
         uint8_t limit_switch_stuck : 1;
         uint8_t motor_slip_encoder_drift : 1;
         uint8_t encoder_error : 1;
     } faults;
-} ;
+};
 
 #define ID_PYRO_STATUS 0x220
-struct PyroStatus_t{
+struct PyroStatus_t
+{
     uint8_t ignitor_cont : 1;
     uint8_t main_cont : 1;
     uint8_t drogue_cont : 1;
-} ;
+};
 
 #define ID_POWER_STATUS 0x230
-struct PowerStatus_t {
+struct PowerStatus_t
+{
     uint16_t voltage_12V_mV;
     uint16_t current_12V_mA;
     uint16_t current_5V_mA;
-    struct {
-        uint8_t external_power_connected:1;
-        uint8_t undervoltage:1;
-        uint8_t overvoltage:1;
-        uint8_t overcurrent_12V:1;
-        uint8_t overcurrent_5V:1;
-        uint8_t shutdown_imminent:1;
-        uint8_t valve_fuse_blown:1;
+    struct
+    {
+        uint8_t external_power_connected : 1;
+        uint8_t undervoltage : 1;
+        uint8_t overvoltage : 1;
+        uint8_t overcurrent_12V : 1;
+        uint8_t overcurrent_5V : 1;
+        uint8_t shutdown_imminent : 1;
+        uint8_t valve_fuse_blown : 1;
     } flags;
-} ;
+};
 
 #define ID_POWER_CONTROL 0x230
-struct PowerControl_t{
-    uint8_t shutdown_request:1;
-} ;
+struct PowerControl_t
+{
+    uint8_t shutdown_request : 1;
+};
 
 #define ID_HEARTBEAT 0x400
-struct Heartbeat_t{
+struct Heartbeat_t
+{
     uint8_t health;
     uint16_t uptime_s;
-} ;
+};
 #define HEALTH_NOMINAL 0
 #define HEALTH_WARNING 1
 #define HEALTH_CRITICAL 2
 
 #define ID_ERROR 0x410
-struct Error_t{
+struct Error_t
+{
     uint8_t error_no;
     uint8_t error_data[7];
-} ;
+};
 
 #define ID_DEBUG_PRINT 0x420
-struct DebugPrint_t{
-    char* text[8];
-} ;
+struct DebugPrint_t
+{
+    char *text[8];
+};
 
-#endif	/* CAN_MESSAGES_H */
-
+#endif /* CAN_MESSAGES_H */
